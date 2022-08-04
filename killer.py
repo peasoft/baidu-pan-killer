a1896ec0c3732efdaf284ba2b3330216af7a72b3
peasoft <luliucheng100@outlook.com>
2022-08-04 21:14:24 +0800

    First.

---
+++ killer.py
@@ -0,0 +1,33 @@
+import random
+import os
+
+char_list = list("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz@#+-=")
+
+def randstr(leng=40):
+    s = ""
+    while leng > 0:
+        s += random.choice(char_list)
+        leng -= 1
+    return s
+
+# Const
+B = 1024
+KB = B * 1024
+MB = KB * 1024
+GB = MB * 1024
+TB = GB * 1024
+
+# Size
+size = 64*GB
+
+while size > 0:
+    filename = randstr(20)
+    with open(filename,'wb') as f:
+        i = 32*MB
+        size -= i
+        while i > 0:
+            f.write(bytes([random.randint(0, 255)]))
+            i -= 1
+    os.system("bypy upload "+filename)
+    os.remove(filename)
+    print("Finished "+filename)
